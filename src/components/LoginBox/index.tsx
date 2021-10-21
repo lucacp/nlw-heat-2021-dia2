37a43eb76ccefd58e5ebfb510b80282ce68ce392
lucacp <lucas_parnoff@hotmail.com>
2021-10-21 01:09:18 -0300

    2ndo dia recem adicionado signOut

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc';
+import { AuthContext } from '../../contexts/auth';
 import styles from './styles.module.scss';
 
+
 export function LoginBox(){
+  const { signInUrl,user} = useContext(AuthContext);
+  console.log(user);
   return (
-    <div className={styles.loginBoxWrapper}>LoginBox</div>
-    <a href="#" className={styles.signInWithGithub}>
-      Entrar com o Github.
-    </a>
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="28" />
+        Entrar com o Github.
+      </a>
+    </div>
   )
 }
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,45 @@
+import styles from './styles.module.scss';
+import { api }   from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+
+type Message = {
+  id: string;
+  text: string;
+  user:{
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList(){
+  const [messages,setMessages] = useState<Message[]>([]);
+  
+  useEffect(() =>{
+    api.get<Message[]>('messages/last3').then(response =>{
+      setMessages(response.data);
+    })
+  },[])
+
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map(message =>{
+          return (
+        <li key={message.id} className={styles.message}>
+          <p className={styles.messageContent}>{message.text}</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src={message.user.avatar_url} alt={message.user.name} />
+            </div>
+          </div>
+          <span>{message.user.name}</span>
+        </li>
+        );
+        })}
+      </ul>
+    </div>
   )
 }
